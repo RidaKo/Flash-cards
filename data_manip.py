de72dc692803b816aaa415d4217928771de79712
RidaKo <ridas.kozukauskas@gmail.com>
2022-12-25 15:55:01 +0200

    Created card flipping and made more functions

--- data_manip.py
+++ data_manip.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import random
 
-def pick_word(words: dict) -> str:
-    return random.choice(words["French"])
 
+def word_generator() ->dict:
+    """
+    ## Word generator
 
-def word_generator() ->str:
+    Returns a dictionary of a french word and an english translation
+    
+    - Use the key **French** to acces french words and **English** to access english words
+
+    """
     with open("french_words.csv", "r" ) as data_file:
-        french_words = pd.read_csv(data_file)
-    return pick_word(french_words)
+        words = pd.read_csv(data_file)
+    random_df_word_row = words.iloc[random.randint(1,100)]
+    return {"French": random_df_word_row["French"],  "English":random_df_word_row["English"]}
--- main.py
+++ main.py
@@ -3,35 +3,47 @@ from data_manip import word_generator
 
 
 
+root = Tk()
+root.config(padx=50, pady=50, bg="Light green")
+
+#create canvas
+canvas = Canvas(width=800, height=530, background="Light green", highlightthickness=0)
+
+#creating the imgaes
+right_image = PhotoImage(file= Image(imgtype = "photo",name = "img/right.png"))
+wrong_image = PhotoImage(file= Image(imgtype="photo", name="img/wrong.png"))
+card_front_img = PhotoImage(file=Image(imgtype="photo",name="img/card_front.png"))
+card_back_img = PhotoImage(file=Image(imgtype="photo",name="img/card_back.png"))\
+
+#put the image in canvas and get the container of the image canvas
+img_container = canvas.create_image(400,265,image = card_front_img)
 
 
-def main() ->None:
+
+def main() -> None:
+    word_pair = word_generator()
+
+    def run()-> None:
+        root.after(3000, flip_card)
+        root.mainloop()
 
     def change_word() ->None:
-        word_label.config(text=word_generator())
+        nonlocal word_pair
+        word_pair = word_generator()
+        canvas.itemconfig(img_container , image = card_front_img)
+        canvas.itemconfig(lang_lab,text ="French", fill = "Black")
+        canvas.itemconfig(word_lab, text=word_pair["French"], fill = "Black")
+        run()
 
     def flip_card():
-        canvas.itemconfig(card_front_img, image = card_back_img)
-        root.after_cancel()
-
-    root = Tk()
-    root.config(padx=50, pady=50, bg="Light green")
-
-    #create canvas
-    canvas = Canvas(width=800, height=530, background="Light green", highlightthickness=0)
-
-    #creating the imgaes
-    right_image = PhotoImage(file= Image(imgtype = "photo",name = "img/right.png"))
-    wrong_image = PhotoImage(file= Image(imgtype="photo", name="img/wrong.png"))
-    card_front_img = PhotoImage(file=Image(imgtype="photo",name="img/card_front.png"))
-    card_back_img = PhotoImage(file=Image(imgtype="photo",name="img/card_back.png"))\
-    
-    #put the image in canvas
-    canvas.create_image(400,265,image = card_front_img)
+        print(word_pair)
+        canvas.itemconfig(img_container , image = card_back_img)
+        canvas.itemconfig(lang_lab,text ="English", fill = "White",)
+        canvas.itemconfig(word_lab,text = word_pair["English"], fill = "White")
 
     #creating the labels
-    language_label = Label(text="Japanese", font=("Arial", 20, "italic"), bg="White", highlightthickness=0)
-    word_label = Label(text="test", font=("Arial", 50, "bold"), bg="White", highlightthickness=0)
+    # language_label = Label(text="Japanese", font=("Arial", 20, "italic"), bg="White", highlightthickness=0)
+    #word_label = Label(text="test", font=("Arial", 50, "bold"), bg="White", highlightthickness=0)
 
 
     #create button
@@ -42,14 +54,12 @@ def main() ->None:
     canvas.grid(column=0, row=0 ,columnspan=2)
     right_button.grid(column=0, row=1)
     wrong_button.grid(column=1, row=1)
-    canvas.create_window(400,50, window= language_label)
-    canvas.create_window(400, 263,window= word_label)
-
-
-    root.after(3000, flip_card)
-
-
-    root.mainloop()
-
+    lang_lab = canvas.create_text(400,50, text="French", font=("Arial", 20, "italic"))
+    word_lab = canvas.create_text(400, 263,text=word_pair["French"], font=("Arial", 50, "bold"))
+    
+    
+    run()
 
 main()
+
+

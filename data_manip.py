9f4517cb26242266e46a3aad6a7e531e25b8df26
RidaKo <ridas.kozukauskas@gmail.com>
2022-12-26 15:25:16 +0200

    Created saving to file and optimized data trasferring

--- data_manip.py
+++ data_manip.py
@@ -1,17 +1,52 @@
 import pandas as pd
 import random
 
-
-def word_generator() ->dict:
+class WordGenerator():
     """
-    ## Word generator
+        ## Word generator class
+        Creates a word generators object
+        The objects primary function is to manipulate words
+    """
+    def __init__(self) -> None:
+        self.words_to_learn = self.read_words_from_file()
+        self.word_pair = None
+        
 
-    Returns a dictionary of a french word and an english translation
+    def read_words_from_file(self) -> object:
+        """
+        Returns a DataFrame object that is filled with all words
+        """
+        with open("french_words.csv", "r" ) as data_file:
+            words = pd.read_csv(data_file)
+            return words
     
-    - Use the key **French** to acces french words and **English** to access english words
+    def remove_words_and_write_to_learnt_file(self) ->None:
+        with open("words_to_learn.csv", "w") as word_file:
+            for word in self.words_to_learn["French"]:
+                if word == self.word_pair["French"]:
+                    # getting the index of the element based on the condition
+                    index = self.words_to_learn.loc[self.words_to_learn["French"]==word].index.to_list()[0]
+                    #print(index)
+                    #print(self.words_to_learn.loc[self.words_to_learn["French"]==word])
+                    self.words_to_learn = self.words_to_learn.drop(index)
+                    print(self.words_to_learn)
+                    self.words_to_learn.to_csv(word_file, index = False, lineterminator ='\n')
+                
+                
+    def generate_words(self) ->None:
+        """
+        ## Word generator
 
-    """
-    with open("french_words.csv", "r" ) as data_file:
-        words = pd.read_csv(data_file)
-    random_df_word_row = words.iloc[random.randint(1,100)]
-    return {"French": random_df_word_row["French"],  "English":random_df_word_row["English"]}
+        Creates a dictionary of a french word and an english translation and adds
+        it as word_pair atribute
+        
+        - Use the key **French** to acces french words and **English** to access english words
+
+        """
+        rand_index_of_row = random.choice([number for number in self.words_to_learn.index.values.tolist()])
+        #print(rand_num)
+        random_df_word_row = self.words_to_learn.loc[rand_index_of_row]
+        #print(random_df_word_row)
+        self.word_pair = {"French": random_df_word_row["French"],  "English":random_df_word_row["English"]}
+
+#random.choice([number for number in self.words_to_learn.index.values.tolist()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from data_manip import word_generator
+from data_manip import WordGenerator
 
 
 
@@ -21,25 +21,34 @@ img_container = canvas.create_image(400,265,image = card_front_img)
 
 
 def main() -> None:
-    word_pair = word_generator()
+    word_gen = WordGenerator() 
+    word_gen.generate_words()
 
+    timer = None
     def run()-> None:
-        root.after(3000, flip_card)
+        nonlocal timer
+        timer = root.after(3000, flip_card)
         root.mainloop()
 
+    def correct_click() ->None:
+        word_gen.remove_words_and_write_to_learnt_file()
+        change_word()
+    
+    def wrong_click() ->None:
+        change_word()
+
     def change_word() ->None:
-        nonlocal word_pair
-        word_pair = word_generator()
+        word_gen.generate_words()
         canvas.itemconfig(img_container , image = card_front_img)
         canvas.itemconfig(lang_lab,text ="French", fill = "Black")
-        canvas.itemconfig(word_lab, text=word_pair["French"], fill = "Black")
+        canvas.itemconfig(word_lab, text=word_gen.word_pair["French"], fill = "Black")
+        root.after_cancel(timer)
         run()
 
     def flip_card():
-        print(word_pair)
         canvas.itemconfig(img_container , image = card_back_img)
         canvas.itemconfig(lang_lab,text ="English", fill = "White",)
-        canvas.itemconfig(word_lab,text = word_pair["English"], fill = "White")
+        canvas.itemconfig(word_lab,text = word_gen.word_pair["English"], fill = "White")
 
     #creating the labels
     # language_label = Label(text="Japanese", font=("Arial", 20, "italic"), bg="White", highlightthickness=0)
@@ -47,15 +56,15 @@ def main() -> None:
 
 
     #create button
-    right_button = Button( image=right_image, background="Light green", command= change_word)
-    wrong_button= Button( image=wrong_image, background="Light green", command= change_word)
+    right_button = Button( image=right_image, background="Light green", command= correct_click)
+    wrong_button= Button( image=wrong_image, background="Light green", command= wrong_click)
 
     # gridding all widgets and putting labels into canvas
     canvas.grid(column=0, row=0 ,columnspan=2)
     right_button.grid(column=0, row=1)
     wrong_button.grid(column=1, row=1)
     lang_lab = canvas.create_text(400,50, text="French", font=("Arial", 20, "italic"))
-    word_lab = canvas.create_text(400, 263,text=word_pair["French"], font=("Arial", 50, "bold"))
+    word_lab = canvas.create_text(400, 263,text= word_gen.word_pair["French"], font=("Arial", 50, "bold"))
     
     
     run()
